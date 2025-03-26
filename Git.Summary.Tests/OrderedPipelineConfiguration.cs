24ea2b8f8ce12c36243a3237c268fdabd960c27a
devizer <devizer@github.io>
2025-03-26 07:03:05 +0200

    up 12, merge and 2nd summary changes, cpu usage and test names

---
+++ Git.Summary.Tests/OrderedPipelineConfiguration.cs
@@ -0,0 +1,57 @@
+﻿using System.Reflection;
+using System.Runtime.Versioning;
+using NUnit.Framework.Interfaces;
+using Universe.NUnitPipeline;
+
+
+// Single shared configuration for all the test assemblies
+public class OrderedPipelineConfiguration
+{
+    public static void Configure()
+    {
+        PipelineLog.LogTrace($"[{typeof(OrderedPipelineConfiguration)}]::Configure()");
+
+        var reportConfiguration = NUnitPipelineConfiguration.GetService<NUnitReportConfiguration>();
+        var reportFile = Path.Combine("TestsOutput", $"Git Summary Tests {GetCurrentNetVersion()}");
+        string artifactsFolder = Environment.GetEnvironmentVariable("SYSTEM_ARTIFACTSDIRECTORY");
+        if (!string.IsNullOrEmpty(artifactsFolder)) reportFile = Path.Combine(artifactsFolder, reportFile);
+        reportConfiguration.InternalReportFile = reportFile;
+
+        var chain = NUnitPipelineConfiguration.GetService<NUnitPipelineChain>();
+
+        chain.OnStart = new List<NUnitPipelineChainAction>()
+        {
+            new() { Title = CpuUsageInterceptor.Title, Action = CpuUsageInterceptor.OnStart },
+            new() { Title = CpuUsageVizInterceptor.Title, Action = CpuUsageVizInterceptor.OnStart },
+        };
+
+        chain.OnEnd = new List<NUnitPipelineChainAction>()
+        {
+            new() { Title = CpuUsageInterceptor.Title, Action = CpuUsageInterceptor.OnFinish },
+            new() { Title = CpuUsageVizInterceptor.Title, Action = CpuUsageVizInterceptor.OnFinish },
+            new() { Title = DisposeInterceptor.Title, Action = DisposeInterceptor.OnFinish },
+            new() { Title = CpuUsageTreeReportInterceptor.Title, Action = CpuUsageTreeReportInterceptor.OnFinish },
+            new() { Title = "Debugger Global Finish", Action = MyGlobalFinish },
+        };
+    }
+
+    private static string GetCurrentNetVersion()
+    {
+        var targetFramework = Assembly.GetExecutingAssembly()
+            .GetCustomAttributes(typeof(TargetFrameworkAttribute))
+            .OfType<TargetFrameworkAttribute>()
+            .FirstOrDefault()
+            ?.FrameworkName;
+
+        var ver = targetFramework?.Split('=')?.Last();
+
+        return ver == null ? "" : $" NET {ver}";
+    }
+
+
+    static void MyGlobalFinish(NUnitStage stage, ITest test)
+    {
+        PipelineLog.LogTrace($"[OrderedPipelineConfiguration.MyGlobalFinish] Finish Type='{test.TestType}' for Test='{test.Name}' on NUnitPipeline");
+    }
+
+}
---
+++ Git.Summary.Tests/OrderedPipelineConfigurationFixture.cs
@@ -0,0 +1,15 @@
+﻿using Universe.NUnitPipeline;
+
+[assembly: NUnitPipelineAction]
+
+// One time setup fixture per each test project
+[SetUpFixture]
+public class OrderedPipelineConfigurationFixture
+{
+    [OneTimeSetUp]
+    public void Configure()
+    {
+        OrderedPipelineConfiguration.Configure();
+    }
+}
+
